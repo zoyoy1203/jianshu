3a4f1761d898bbd20407590330e618fca7075019
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-01-06 22:20:30 +0800

    首页开发中

--- src/pages/home/components/Writer.js
+++ src/pages/home/components/Writer.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { 
+    WriterWrapper,
+    WriterTitle,
+    WriterSwitch
+} from '../style';
 class Writer extends Component {
     render() {
+       
         return (
-            <div>Writer</div>
+            <WriterWrapper>
+                    <WriterTitle>
+                    热门搜索
+                    <WriterSwitch >
+                    <span ref={(icon) => {this.spinIcon = icon}} className={ 'iconfont spin'}>&#xe851;</span>
+                    换一批
+                    </WriterSwitch>
+                </WriterTitle>
+            </WriterWrapper>
         )
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+   
+    }
+}
+const mapDispathToProps = (dispatch) => {
+    return {
+    
 
-export default Writer;
+    }
+}
+export default connect(mapStateToProps, mapDispathToProps)(Writer);
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -14,7 +14,77 @@ export const HomeLeft = styled.div`
 `
 
 export const HomeRight = styled.div`
-    width: 240px;
+    width: 280px;
     float: right;
 `
 
+export const ListItem = styled.div`
+    overflow: hidden;
+    padding: 20px 0;
+    border-bottom: 1px solid #dcdcdc;
+    .pic {
+        width: 125px;
+        height: 100px;
+        display: block;
+        float: right;
+        border-radius: 10px;
+    }
+`
+
+export const ListInfo = styled.div`
+    width: 500px;
+    float: left;
+    .title {
+        line-height: 27px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #333;
+    }
+    .desc {
+        font-size: 13px;
+        line-height: 18px;
+        color: #999;
+    }
+`
+
+export const RecommendWrapper = styled.div`
+    margin: 30px 0;
+    width: 280px;
+`
+export const RecommendItem = styled.div`
+    cursor: pointer;
+    width: 280px;
+    height: 50px;
+    background: url(${(props) => props.imgUrl});
+    background-size: contain;
+`
+
+export const WriterWrapper = styled.div`
+    width: 278px;
+    border: 1px solid #dcdcdc;
+    border-radius: 3px;
+    height: 300px;
+    line-height: 300px;
+    text-align: center;
+`
+export const WriterTitle = styled.div`
+    margin-top: 20px;
+    margin-bottom: 15px;
+    line-height: 20px;
+    font-size: 14px;
+    color: #969696;
+`
+
+export const WriterSwitch = styled.div`
+    float: right;
+    font-size: 13px;
+    cursor: pointer;
+    .spin {
+        display: block;
+        float: left;
+        font-size: 12px;
+        margin-right: 2px;
+        transition: all .2s ease-in;
+        transfrom-origin: center center;
+    }
+`
