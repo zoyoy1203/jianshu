1a5ea3b6d1c441cfc0830fe64d3a73d932486e6b
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-01-07 16:12:30 +0800

    详情页面基本结构完成

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { DetailWrapper, Header, Content } from './style';
+import { actionCreators } from './store';
 class Detail extends Component {
     render() {
         return (
-            <div>Detail</div>
+            <DetailWrapper>
+                <Header>{this.props.title}</Header>
+                <Content dangerouslySetInnerHTML={{__html: this.props.content}} />
+            </DetailWrapper>
         )
+    }
+
+    componentDidMount() {
+        this.props.getDetail(this.props.match.params.id);
     }
 }
 
+const mapState = (state) => ({
+    title: state.getIn(['detail','title']),
+    content: state.getIn(['detail','content'])
+});
 
-export default Detail;
+const mapDispatch = (dispatch) => ({
+    getDetail(id) {
+        dispatch(actionCreators.getDetail(id))
+    }
+})
+export default connect(mapState,mapDispatch)(Detail);
---
+++ src/pages/detail/store/actionCreators.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+const changeDetail = (title, content) => ({
+    type: actionTypes.CHANGE_DETIAL,
+    title,
+    content
+})
+export const getDetail = (id) => {
+    return (dispatch) => {
+        axios.get('/api/detail.json?id='+ id).then((res) => {
+            const result = res.data.data;
+            dispatch(changeDetail(result.title,result.content))
+        })
+    }
+}
